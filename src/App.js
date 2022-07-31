d9f69e8263716242677129fc78771637adecdc02
dev-alfaiz <captainalfaiz@gmail.com>
2022-07-31 14:18:10 +0530

    Initial Push

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1>React Redux-Toolkit CRUD</h1>
+    </div>
+  );
+};
+
+export default App;
